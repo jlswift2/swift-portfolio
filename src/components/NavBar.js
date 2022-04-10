ddb6006d4cc0a3f3fa8f9b9316e211ef4ed395c3
Justin Swift <jlswift2@gmail.com>
2022-04-09 21:14:56 -0500

    Add styling to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,26 +3,28 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="pl-8">
-        <ul className="flex space-x-4 text-grey-900 flex-col" id="navList">
-            <li className="hover:underline hover:text-red-500">
-                <NavLink to="/">Home</NavLink>
-            </li>
-            <li className="hover:underline hover:text-red-500">
-                <NavLink to="/about">About</NavLink>
-            </li>
-            <li className="hover:underline hover:text-red-500">
-                <NavLink to="/portfolio">Portfolio</NavLink>
-            </li>
-            <li className="hover:underline hover:text-red-500">
-                <NavLink to="/blogs">Blog</NavLink>
-            </li>
-            <li className="hover:underline hover:text-red-500">
-                <NavLink to="/contact">Contact</NavLink>
-            </li>
-                
-        </ul>
-    </div>
+    <nav class="fixed top-0 left-0 h-screen w-72 flex flex-col bg-gray-900 shadow text-white">
+        <div >
+            <ul  id="navList">
+                <li className="hover:underline hover:text-red-500">
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li className="hover:underline hover:text-red-500">
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li className="hover:underline hover:text-red-500">
+                    <NavLink to="/portfolio">Portfolio</NavLink>
+                </li>
+                <li className="hover:underline hover:text-red-500">
+                    <NavLink to="/blogs">Blog</NavLink>
+                </li>
+                <li className="hover:underline hover:text-red-500">
+                    <NavLink to="/contact">Contact</NavLink>
+                </li>
+                    
+            </ul>
+        </div>
+    </nav>
   )
 }
 
